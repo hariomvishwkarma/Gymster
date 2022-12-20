2e68d00da5f1d60b20512184beb7dac76fba85a2
hariomvishwakarma1 <117162788+hariomvishwakarma1@users.noreply.github.com>
2022-12-20 15:28:06 +0530

    gymster

---
+++ assets/js/style.js
@@ -0,0 +1,56 @@
+// // page hide show 
+// $(document).ready(function() {
+//     $(".dropdown ").click(function() {
+//         $(".toggle-click ").toggle();
+//     });
+// });
+// // toggle hide show
+// $(document).ready(function() {
+//     $(".hamburger-toggle ").click(function() {
+//         $(".responsive-navbar ").toggle(1000);
+//     });
+// });
+// // slick slider for image banner
+// $(document).ready(function() {
+//     $('.main-image-banner').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: true,
+//         dots: true,
+//         speed: 1800,
+//         infinite: true,
+//         autoplaySpeed: 5000,
+//         autoplay: false,
+//     });
+// });
+
+// // about button
+// $('.link-about-us1').on('click', function() {
+//     $(this).addClass('is-active');
+//     $(".about-peragraph").hide();
+//     $(".about-peragraph1").show();
+//     $('.link-about-us').removeClass('is-active');
+// });
+
+// $('.link-about-us').on('click', function() {
+//     $(this).addClass('is-active');
+//     $(".about-peragraph1").hide();
+//     $(".about-peragraph").show();
+//     $('.link-about-us1').removeClass('is-active');
+// });
+
+// // end about button
+
+// // week day change
+// $('.week-day-name').on('click', function() {
+//     $(this).addClass('active-day-name');
+//     $('.week-day-name').removeClass('active-day-name');
+//     $(this).addClass('active-day-name');
+// });
+
+// $(document).ready(function() {
+//     $(".week-day-name").click(function() {
+//         $(".main-container-time-name-teacher").fadeOut();
+//         $(".main-container-time-name-teacher").fadeIn();
+//     });
+// });
